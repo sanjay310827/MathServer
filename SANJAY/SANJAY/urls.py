565835b8b11f947873d5acb664c7d86dea8af9c8
sanjay310827 <sanjaymaddy31@gmail.com>
2025-10-04 22:57:19 +0530

    Success

---
+++ SANJAY/SANJAY/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from ownapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('bmi/',views.calculate_bmi,name="bmi"),
+    path('',views.calculate_bmi,name="bmicalculator")
+]
